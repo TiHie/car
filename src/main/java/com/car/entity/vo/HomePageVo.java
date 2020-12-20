e015ae23cd46bd6c981365f1277f1d851629eb17
xx20000306 <445094340@qq.com>
2020-12-20 16:53:01 +0800

    首页数据展示接口完成

---
+++ src/main/java/com/car/entity/vo/HomePageVo.java
@@ -0,0 +1,21 @@
+package com.car.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author mowuwalixilo
+ * @date2020/12/20 16:39
+ */
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class HomePageVo {
+
+    private Integer id;
+    private String name;
+    private String image;
+}
